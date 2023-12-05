739879ae6a85302ab9114931fc67b5f18f98319f
AzureAD\OlaBarecka <o.barecka@gmail.com>
2023-12-05 07:42:18 +0000

    people view

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <script setup>
 import { RouterLink, RouterView } from 'vue-router';
+import { RouteNames } from '@/router/RouteNames';
 </script>
 
 <template>
@@ -7,7 +8,7 @@ import { RouterLink, RouterView } from 'vue-router';
     <div class="wrapper">
       <nav>
         <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
+        <RouterLink :to="{ name: RouteNames.PEOPLE }">Test me</RouterLink>
       </nav>
     </div>
   </header>
--- src/router/index.js
+++ src/router/index.js
@@ -1,21 +1,29 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import HomeView from '../views/HomeView.vue';
+import { RouteNames } from './RouteNames';
+
+const PeopleView = () => import('../views/PeopleView.vue');
+const PersonEdit = () => import('../views/PeopleView.vue');
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
-      name: 'home',
+      name: RouteNames.HOME,
       component: HomeView,
     },
     {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue'),
+      path: '/people',
+      name: RouteNames.PEOPLE,
+      component: PeopleView,
+      children: [
+        {
+          path: ':personId/edit',
+          name: RouteNames.PERSON_EDIT,
+          component: PersonEdit,
+        },
+      ],
     },
   ],
 });
