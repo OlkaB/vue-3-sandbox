7cba8bcfe0c0842ea322e50740804993500b2498
AzureAD\OlaBarecka <o.barecka@gmail.com>
2023-12-05 07:21:48 +0000

    initial setup

---
+++ .eslintrc.js
@@ -0,0 +1,23 @@
+/* eslint-env node */
+require('@rushstack/eslint-patch/modern-module-resolution');
+
+module.exports = {
+  root: true,
+  'extends': [
+    'plugin:vue/vue3-recommended',
+    'eslint:recommended',
+    '@vue/eslint-config-prettier/skip-formatting',
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+  },
+  rules: {
+    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'vue/multi-word-component-names': 'off',
+    'function-paren-newline': ['error', 'consistent'],
+    'default-param-last': 'warn',
+    'semi': ['error', 'always'],
+    'comma-dangle': ['error', 'always-multiline'],
+  },
+};
