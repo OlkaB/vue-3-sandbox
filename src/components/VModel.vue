056712bca555d84c80b850add9f69bdfc5c39999
AzureAD\OlaBarecka <o.barecka@gmail.com>
2023-12-10 14:00:42 +0000

    v-model test

---
+++ src/components/VModel.vue
@@ -0,0 +1,59 @@
+<template>
+  <div class="">
+    <div class="label">
+      Base v-model
+    </div>
+    <input
+      :value="modelValue"
+      class="input"
+      @input="emitInput"
+    />
+
+    <br>
+
+    <input
+      :value="syncA"
+      class="input"
+      @input="$emit('update:sync-a', $event.target.value)"
+    />
+
+    <br>
+
+    <input
+      :value="syncB"
+      class="input"
+      @input="$emit('update:sync-b', $event.target.value)"
+    />
+  </div>
+</template>
+
+<script>
+// intentionally options API
+export default {
+  props: {
+    modelValue: {
+      type: String,
+      default: '',
+    },
+    syncA: String,
+    syncB: String,
+  },
+  emits: ['update:modelValue', 'update:sync-a', 'update:sync-b'],
+  methods: {
+    emitInput($event) {
+      this.$emit('update:modelValue', $event.target.value);
+    },
+  },
+};
+</script>
+
+<style>
+.label {
+ font-weight: bold;
+ margin-bottom: .25rem;
+}
+
+.input {
+  margin-bottom: 1rem;
+}
+</style>
---
+++ src/views/TestView.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="test">
+    <h1>This is a test page</h1>
+    <br>
+
+    <VModel v-model="baseModelValue" v-model:sync-a="syncA" v-model:sync-b="syncB" ></VModel>
+    <br>
+
+    1. baseModelValue: {{ baseModelValue }}
+    <br>
+    2. syncA: {{ syncA }}
+    <br>
+    3. syncB: {{ syncB }}
+  </div>
+</template>
+
+<script>
+// intentionally options API
+import VModel from '@/components/VModel.vue';
+
+export default {
+  components: {
+    VModel,
+  },
+  data()  {
+    return {
+      baseModelValue: '',
+      syncA: '',
+      syncB: '',
+    };
+  },
+};
+</script>
+
+<style>
+@media (min-width: 1024px) {
+  .about {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+}
+</style>
